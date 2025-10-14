83a7d8653b0cfad50e09676afd0662014a84e735
JoelLeyes <joel.leyes@estudiantes.utec.edu.uy>
2025-10-13 22:42:53 -0300

    datos de conexion de la BDD

--- BackEnd/backend/settings.py
+++ BackEnd/backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'proyecto_registro_usuarios',
+        'USER': 'postgres',
+        'PASSWORD': 'yoelymale',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
